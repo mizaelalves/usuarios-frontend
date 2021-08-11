f259b673f10d73fdc0d91d366f5241408f4de524
Mizael Alves <mizaelbna@hotmail.com>
2021-08-11 03:04:56 -0300

    fix

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Datatable from "../datatable/index";
+import Datatable from "./datatable/index";
 import "./style.css";
 
 require("es6-promise").polyfill();
@@ -9,9 +9,9 @@ export default function App() {
   const [data, setData] = useState([]);
   const [q, setQ] = useState("");
 
-  function search(rows) {
+  async function search(rows) {
     const columns = rows[0] && Object.keys(rows[0]);
-    return rows.filter((row) =>
+    return await rows.filter((row) =>
       columns.some(
         (column) =>
           row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
@@ -19,6 +19,7 @@ export default function App() {
     );
   }
 
+  
   useEffect(() => {
     fetch(`http://localhost:5000/users`)
       .then((response) => response.json())
@@ -30,9 +31,6 @@ export default function App() {
       <div>
         <div className="search">
           <input className="searchTerm" type="text" value={q} onChange={(e) => setQ(e.target.value)} />
-        </div>
-        <div className="button">
-          <button className="searchButton">Criar usuário</button>
         </div>
         <div className='table'>
           <Datatable data={search(data)} />
